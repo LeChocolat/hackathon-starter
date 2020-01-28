97b86e23b0a9289125c35886ab9b37d96572347a
LeChocolat <rachaelr1.rr@gmail.com>
2020-01-27 23:22:47 -0600

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -84,9 +84,9 @@ function sshConnect() {
     ssh
         .connect({
             // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-            host: '18.234.208.163',
+            host: '52.202.75.222',
             username: 'ubuntu',
-            privateKey: 'hs-key.pem'
+            privateKey: 'sna-key'
         })
         .then(function() {
             console.log('SSH Connection established.');
